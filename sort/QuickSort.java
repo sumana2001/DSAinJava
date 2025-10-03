f04951443e2c3e6880c51692ac8cde209027243b
sumana2001 <sumana.basu2001@gmail.com>
2025-10-03 07:10:54 +0530

    quick sort added

---
+++ sort/QuickSort.java
@@ -0,0 +1,48 @@
+package sort;
+
+import java.util.Scanner;
+
+public class QuickSort {
+    public static int partition(int[] arr, int low, int high){
+        int pivot=arr[low];
+        int i=low;
+        int j=high;
+        while(i<j){
+            while(arr[i]<=pivot && i<high)
+                i++;
+            while(arr[j]>pivot && j>low)
+                j--;
+            if(i<j){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+        int temp = arr[low];
+        arr[low] = arr[j];
+        arr[j] = temp;
+        return j;
+    }
+
+    public static void quickSort(int[] arr,int low, int high){
+        if(low>=high){
+            return;
+        }
+        int part=partition(arr,low,high);
+        quickSort(arr, low,part-1);
+        quickSort(arr, part+1, high);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        PrintAnyArray.printAnyArrayStatic(arr);
+        quickSort(arr, 0, n-1);
+        PrintAnyArray.printAnyArrayStatic(arr);
+        sc.close();
+    }
+}
