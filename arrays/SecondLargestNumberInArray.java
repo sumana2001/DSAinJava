85bd04195ec22ded9bf0202964c354f55b2d7532
sumana2001 <sumana.basu2001@gmail.com>
2025-10-04 07:47:19 +0530

    largest num in array added

---
+++ arrays/SecondLargestNumberInArray.java
@@ -0,0 +1,29 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class SecondLargestNumberInArray {
+    public static int secondLargestNumberInArray(int[] arr, int n){
+        int max=-1;
+        int smax=-1;
+        for(int i=0;i<n;i++){
+            if(arr[i]>max){
+                smax=max;
+                max=arr[i];
+            } else if(arr[i]>smax && arr[i]<max){
+                smax=arr[i];
+            }
+        }
+        return smax;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println(secondLargestNumberInArray(arr,n));
+        sc.close();
+    }
+}
