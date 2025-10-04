bc75ca5bedd4474c684e95a6aafbcdb7035811d3
sumana2001 <sumana.basu2001@gmail.com>
2025-10-04 16:21:27 +0530

    Move zeroes added

---
+++ arrays/MoveZeroes.java
@@ -0,0 +1,38 @@
+package arrays;
+
+import java.util.Scanner;
+
+import sort.PrintAnyArray;
+
+public class MoveZeroes {
+    public static void moveZeroes(int[] arr, int n){
+        int i=0;
+        while(i<n && arr[i]!=0){
+            i++;
+        }
+        int j=i+1;
+        while(j<n){
+            if(arr[j]!=0){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        PrintAnyArray.printAnyArrayStatic(arr);
+        moveZeroes(arr,n);
+        PrintAnyArray.printAnyArrayStatic(arr);
+        sc.close();
+    }
+}
