99f4c0e36c2ef0ba6230edc7460a0d38e1c2846f
sumana2001 <sumana.basu2001@gmail.com>
2025-10-05 12:01:58 +0530

    array questions added

---
+++ arrays/FindMaxConsecutiveOnes.java
@@ -0,0 +1,30 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class FindMaxConsecutiveOnes {
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int n=nums.length;
+        int maxCount=0,count=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==1){
+                count++;
+            } else {
+                maxCount=Math.max(count,maxCount);
+                count=0;
+            }
+        }
+        maxCount=Math.max(count,maxCount);
+        return maxCount;
+    }   
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println(findMaxConsecutiveOnes(arr));
+        sc.close();
+    }
+}
--- arrays/MissingNumber.java
+++ arrays/MissingNumber.java
@@ -2,24 +2,24 @@ package arrays;
 
 import java.util.Scanner;
 
-public class LinearSearch {
-    public static int search(int arr[], int n, int x) {
-        for (int i=0;i<n;i++){
-            if(arr[i]==x){
-                return i;
-            }
+public class MissingNumber {
+    public static int missingNumber(int[] nums) {
+        int n=nums.length;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
         }
-        return -1;
+        int actualSum=(n*(n+1))/2;
+        return actualSum-sum;
     }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int x=sc.nextInt();
         int n=sc.nextInt();
         int arr[]=new int[n];
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
-        System.out.println(search(arr,n,x));
+        System.out.println(missingNumber(arr));
         sc.close();
     }
 }
